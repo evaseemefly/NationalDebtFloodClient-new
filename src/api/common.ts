b47bf7edd9037f1142bef742da480f55365f1751
evaseemefly <evaseemefly@126.com>
2025-07-10 10:51:21 +0800

    title:25-07-10 api common修改为pc地址

--- src/api/common.ts
+++ src/api/common.ts
@@ -3,7 +3,8 @@
 // 全球预报监测
 // export const host = 'http://172.16.30.163:92'
 // export const host = 'http://128.5.10.21:92'
-export const host = 'http://localhost:92'
+// export const host = 'http://localhost:92'
+export const host = 'http://124.17.0.204:8075'
 
 /** @type {*} 21-03-05 + 新添加的用来加载 geoserver的host */
 // export const hostGeo = 'http://128.5.10.21:8082/geoserver/'
